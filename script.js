d837be5c1f8661d2d9bb20dd198a2f086b2e583f
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-06-24 14:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const getCurrentWeather = () => {
+			const url =
+				"https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de";
+
+			fetch(url)
+				.then((response) => response.json())
+				.then((data) => {
+					const weatherData = document.getElementById("weatherData");
+					weatherData.textContent = `Current weather in London: ${data.weather[0].main}`;
+				})
+				.catch((error) => console.error(error));
+		};
